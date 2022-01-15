7d96a603d2df0ee9bc80859035f1bc5a6ad9d482
Ríkard Alves <96666372+RikardSant@users.noreply.github.com>
2022-01-15 13:40:35 -0300

    Refazendo o projeto battle

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# python
+<h1>python</h1>
+<p>Este é meu repositório do curso de Python</p>
  
---
+++ projetos_pessoais/rpg_battle/library/classes.py
@@ -0,0 +1,5 @@
+class Human:
+    __slots__ = ['_name']
+
+    def __int__(self, name):
+        self._name = name
